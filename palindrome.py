3072036542b6d2bb235eed9012f0eb7dcf67ecf8
AaftabPatel <99026627+AaftabPatel@users.noreply.github.com>
2022-05-05 17:10:55 +0530

    Delete palindrome.py

--- palindrome.py
+++
@@ -1,7 +0,0 @@
-print("program to check palindrome")
-s=input("Please enter the value to check:")
-rev=s[::-1]
-if(s==rev):
-    print(s,"is a palindrome")
-else:
-    print(s,"is not a palindrome")
