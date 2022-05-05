b597a4e926141e21b59cd559dd0ff974a93ba4d6
AaftabPatel <99026627+AaftabPatel@users.noreply.github.com>
2022-05-05 17:10:02 +0530

    Delete money_conversion.py

--- money_conversion.py
+++
@@ -1,9 +0,0 @@
-print("program to covert amount of dollars into Rs.:")
-
-n=float(input("please enetr the amount of money in dollar: "))
-
-def calc(n): 
-    a = n*75 
-    return a
-a=calc(n)
-print("MOney in rupees is:",a)
