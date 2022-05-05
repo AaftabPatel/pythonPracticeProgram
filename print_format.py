03f77d8d60a871db55928c8ad21e92534d3483d4
AaftabPatel <99026627+AaftabPatel@users.noreply.github.com>
2022-05-05 17:12:15 +0530

    Delete print_format.py

--- print_format.py
+++
@@ -1,9 +0,0 @@
-#program for table generation
-n=int(input("Please enter value whose table you want:"))
-print("Table of",n,"is:")
-m=n*10
-count=1
-for i in range(n,m+1,n):
-    print("{}*{}={}".format(n,count,i))
-    count+=1
-    
