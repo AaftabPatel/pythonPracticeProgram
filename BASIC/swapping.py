88e0bd7e9fc27ddf1d43777c8475d900dc7b65e5
AaftabPatel <99026627+AaftabPatel@users.noreply.github.com>
2022-05-10 19:35:10 +0530

    Add files via upload

---
+++ BASIC/swapping.py
@@ -0,0 +1,7 @@
+print("A PROGRAM TO PERFORM SWAPING WITHOUT USING TEMP VARIABLE")
+n1=int(input("please enter first no."))
+n2=int(input("please enter second no."))
+n1,n2=n2,n1
+print("after swapping:")
+print("first no. is:",n1)
+print("seccond no. is:",n2)
