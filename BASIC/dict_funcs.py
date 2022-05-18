ba6fe8d7de579cc0cd859c1ceafcf874b7b1fc49
AaftabPatel <99026627+AaftabPatel@users.noreply.github.com>
2022-05-18 16:59:17 +0530

    Add files via upload

---
+++ BASIC/dict_funcs.py
@@ -0,0 +1,12 @@
+print("program for dictionary funtions")
+d={1:'a',2:'e',3:'i',4:'o',5:'u'}
+print("entersd tuple is",d)
+print("length of dictionary is: ",len(d))
+print("1 of tupldictionary is: ",d.get(1))
+print("items of dictionary is: ",d.items())
+print("keys of dictionary is: ",d.keys())
+print("values of dictionary is: ",d.values())
+d2={6:'aa'}
+print("dictionary after update b is: ",d.update(d2))
+print("dictionary after clear  is: ",d.clear())
+
